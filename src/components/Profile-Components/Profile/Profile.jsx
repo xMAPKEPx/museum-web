d24a8d7c393f97f48e7521957cc4e5da0a4195ca
Andrew <russia201554321@gmail.com>
2024-07-08 04:12:00 +0500

    fix

--- src/components/Profile-Components/Profile/Profile.jsx
+++ src/components/Profile-Components/Profile/Profile.jsx
@@ -86,116 +86,122 @@ function Profile() {
                 <h1 className={styles.profileTitle}>Профиль</h1>
 
                 <form onSubmit={handleSubmit} className={styles.profileContentBox}>
-                <div className={styles.profileFlexCol1}>
-                    <div className={styles.profileFlexRow}>
-                        <div className={styles.profileFlexCol2}>
-                            <div className={styles.profileFlexCol3}>
-                                <label htmlFor='last_name' className={styles.inputLabel}>Фамилия</label>
-                                <input
-                                    name="last_name"
-                                    id="last_name"
-                                    className={styles.input}
-                                    disabled={!isChanging}
-                                    onChange={handleChange}
-                                    value={user.last_name}/>
-                                <hr className={styles.profileLine1} size={1}/>
-                            </div>
-
-                            <div className={styles.profileFlexCol3}>
-                                <label htmlFor='first_name' className={styles.inputLabel}>Имя</label>
-                                <input
-                                    name="first_name"
-                                    id="first_name"
-                                    className={styles.input}
-                                    disabled={!isChanging}
-                                    onChange={handleChange}
-                                    value={user.first_name}/>
-                                <hr className={styles.profileLine1} size={1}/>
-                            </div>
-
-                            <div className={styles.profileFlexCol3}>
-                                <label htmlFor='email' className={styles.inputLabel}>E-mail</label>
-                                <input
-                                    type="email"
-                                    name="email"
-                                    id="email"
-                                    className={styles.input}
-                                    disabled={!isChanging}
-                                    onChange={handleChange}
-                                    value={user.email}/>
-                                <hr className={styles.profileLine1} size={1}/>
-                            </div>
-
-                            <div className={styles.profileFlexCol4}>
-                                <label htmlFor='password' className={styles.inputLabel}>Пароль</label>
-
-                                <div className={styles.profilePasswordFlexRow}>
+                    <div className={styles.profileFlexCol1}>
+                        <div className={styles.profileFlexRow}>
+                            <div className={styles.profileFlexCol2}>
+                                <div className={styles.profileFlexCol3}>
+                                    <label htmlFor='last_name' className={styles.inputLabel}>Фамилия</label>
                                     <input
-                                        type={isShown ? 'text' : 'password'}
-                                        name="password"
-                                        id="password"
+                                        name="last_name"
+                                        id="last_name"
                                         className={styles.input}
                                         disabled={!isChanging}
                                         onChange={handleChange}
-                                        value={user.password}/>
-                                    <img onClick={showPassword}
-                                         className={styles.profilePasswordImage}
-                                         src={eye}
-                                         alt="alt text"
-                                    />
+                                        value={user.last_name}/>
+                                    <hr className={styles.profileLine1} size={1}/>
                                 </div>
 
-                                <hr className={styles.profileLine1} size={1}/>
-                            </div>
+                                <div className={styles.profileFlexCol3}>
+                                    <label htmlFor='first_name' className={styles.inputLabel}>Имя</label>
+                                    <input
+                                        name="first_name"
+                                        id="first_name"
+                                        className={styles.input}
+                                        disabled={!isChanging}
+                                        onChange={handleChange}
+                                        value={user.first_name}/>
+                                    <hr className={styles.profileLine1} size={1}/>
+                                </div>
 
-                            <div className={styles.profileFlexCol3}>
-                                <label htmlFor='phone' className={styles.inputLabel}>Контактный номер</label>
+                                <div className={styles.profileFlexCol3}>
+                                    <label htmlFor='email' className={styles.inputLabel}>E-mail</label>
+                                    <input
+                                        type="email"
+                                        name="email"
+                                        id="email"
+                                        className={styles.input}
+                                        disabled={!isChanging}
+                                        onChange={handleChange}
+                                        value={user.email}/>
+                                    <hr className={styles.profileLine1} size={1}/>
+                                </div>
+
+                                <div className={styles.profileFlexCol4}>
+                                    <label htmlFor='password' className={styles.inputLabel}>Пароль</label>
+
+                                    <div className={styles.profilePasswordFlexRow}>
+                                        <input
+                                            type={isShown ? 'text' : 'password'}
+                                            name="password"
+                                            id="password"
+                                            className={styles.input}
+                                            disabled={!isChanging}
+                                            onChange={handleChange}
+                                            value={user.password}/>
+                                        <img onClick={showPassword}
+                                             className={styles.profilePasswordImage}
+                                             src={eye}
+                                             alt="alt text"
+                                        />
+                                    </div>
+
+                                    <hr className={styles.profileLine1} size={1}/>
+                                </div>
+
+                                <div className={styles.profileFlexCol3}>
+                                    <label htmlFor='phone' className={styles.inputLabel}>Контактный номер</label>
+                                    <input
+                                        type="tel"
+                                        name="phone"
+                                        id="phone"
+                                        className={styles.input}
+                                        disabled={!isChanging}
+                                        onChange={handleChange}
+                                        value={user.phone}/>
+                                    <hr className={styles.profileLine1} size={1}/>
+                                </div>
+                            </div>
+                            <div className={styles.profileFlexCol5}>
+                                {photo ? <img
+                                        className={styles.profileImage}
+                                        src={URL.createObjectURL(photo)}
+                                        alt="alt text"
+                                    /> :
+                                    <img
+                                        className={styles.profileImage}
+                                        src={user.image_url ? user.image_url : image}
+                                        alt="alt text"
+                                    />}
+                                <button className={styles.profileChangeButton} onClick={() => setPhoto(null)}>Очистить
+                                </button>
+                                <label className={styles.selectButton} htmlFor='myImage'>Выбрать фото</label>
                                 <input
-                                    type="tel"
-                                    name="phone"
-                                    id="phone"
-                                    className={styles.input}
-                                    disabled={!isChanging}
-                                    onChange={handleChange}
-                                    value={user.phone}/>
-                                <hr className={styles.profileLine1} size={1}/>
+                                    className={styles.hidden}
+                                    type="file"
+                                    name="myImage"
+                                    id="myImage"
+                                    // Event handler to capture file selection and update the state
+                                    onChange={(event) => {
+                                        setPhoto(event.target.files[0])
+                                    }}
+                                />
+                                <button onClick={handleImgChange} className={styles.profileChangeButton}>Изменить
+                                </button>
                             </div>
                         </div>
-                        <div className={styles.profileFlexCol5}>
-                            {photo ? <img
-                                    className={styles.profileImage}
-                                    src={URL.createObjectURL(photo)}
-                                    alt="alt text"
-                                /> :
-                                <img
-                                    className={styles.profileImage}
-                                    src={user.image_url ? user.image_url : image}
-                                    alt="alt text"
-                                />}
-                            <button className={styles.profileChangeButton} onClick={() => setPhoto(null)}>Очистить
-                            </button>
-                            <label className={styles.selectButton} htmlFor='myImage'>Выбрать фото</label>
-                            <input
-                                className={styles.hidden}
-                                type="file"
-                                name="myImage"
-                                id="myImage"
-                                // Event handler to capture file selection and update the state
-                                onChange={(event) => {
-                                    setPhoto(event.target.files[0])
-                                }}
-                            />
-                            <button onClick={handleImgChange} className={styles.profileChangeButton}>Изменить</button>
-                        </div>
+                        <button className={styles.navButton} onClick={() => navigate('/me/collections')}>К моим
+                            коллекциям
+                        </button>
+                        <button className={styles.navButton} onClick={() => window.location.assign('https://kit.vstrechya.space/me/exhibitions')}>К моим
+                            выставкам
+                        </button>
+                        {isChanging ? <button type="submit"
+                                              className={styles.profileEditButton}>Сохранить</button> :
+                            <button onClick={handleClick} className={styles.profileEditButton}
+                                    disabled={isSending}>Редактировать</button>}
+                        <button className={styles.profileEditButton} onClick={e => handleLogOut(e)}>Выйти</button>
                     </div>
-                    <button className={styles.navButton} onClick={() => navigate('/me/collections')}>К моим коллекциям</button>
-                    {isChanging ? <button type="submit"
-                                          className={styles.profileEditButton}>Сохранить</button> :
-                        <button onClick={handleClick} className={styles.profileEditButton}
-                                disabled={isSending}>Редактировать</button>}
-                    <button className={styles.profileEditButton} onClick={e => handleLogOut(e)}>Выйти</button>
-                </div>
-            </form>
+                </form>
             </div>
         </div>
     </>
